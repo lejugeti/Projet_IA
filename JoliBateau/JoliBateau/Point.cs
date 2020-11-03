3114ca69007956541527d118f0dcdf6241ade350
Antoine PARIZE <lejugeti@gmail.com>
2020-11-03 10:16:14 +0100

    revert

--- JoliBateau/JoliBateau/Point.cs
+++ JoliBateau/JoliBateau/Point.cs
@@ -236,11 +236,56 @@ namespace JoliBateau
             // en plus on enlève la restriction des 10km ça rend le calcul plus facile plutôt que de calculer la trajectoire optimale avec 
             // les diagonales.
 
+<<<<<<< HEAD
             double distance = Math.Sqrt(Math.Pow((X - Pf.X), 2) + Math.Pow((Y - Pf.Y), 2));
             double windspeed = 50;
             double boatspeed = 0.9 * windspeed; // vitesse du bateau avec meilleur angle par rapport au vent
+=======
+            Point Pf = Point.PointFinal();
+            double x1 = X;
+            double y1 = Y;
+            double x2 = Pf.X;
+            double y2 = Pf.Y;
 
+            /*double distance = Math.Sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
+            double windspeed = 50;
+            double alpha = 45;
+            double boatspeed = (0.9 - 0.2 * (alpha - 45) / 45) * windspeed;
+           
+            return (distance / boatspeed);*/
+>>>>>>> parent of c0e6927... modif hcost
+
+            double distance = Math.Sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
+            double windspeed = Vent.GetWindSpeed((x1 + x2) / 2.0, (y1 + y2) / 2.0);
+            double winddirection = Vent.GetWindDirection((x1 + x2) / 2.0, (y1 + y2) / 2.0);
+            double boatspeed;
+            double boatdirection = Math.Atan2(y2 - y1, x2 - x1) * 180 / Math.PI;
+            // On ramène entre 0 et 360
+            if (boatdirection < 0) boatdirection = boatdirection + 360;
+            // calcul de la différence angulaire
+            double alpha = Math.Abs(boatdirection - winddirection);
+            // On se ramène à une différence entre 0 et 180 :
+            if (alpha > 180) alpha = 360 - alpha;
+            if (alpha <= 45)
+            {
+                /* (0.6 + 0.3α / 45) v_v */
+                boatspeed = (0.6 + 0.3 * alpha / 45) * windspeed;
+            }
+            else if (alpha <= 90)
+            {
+                /*v_b=(0.9-0.2(α-45)/45) v_v */
+                boatspeed = (0.9 - 0.2 * (alpha - 45) / 45) * windspeed;
+            }
+            else if (alpha < 150)
+            {
+                /* v_b=0.7(1-(α-90)/60) v_v */
+                boatspeed = 0.7 * (1 - (alpha - 90) / 60) * windspeed;
+            }
+            else
+                return 1000000;
+            // estimation du temps de navigation entre p1 et p2
             return (distance / boatspeed);
+
         }
 
         public override string ToString()
